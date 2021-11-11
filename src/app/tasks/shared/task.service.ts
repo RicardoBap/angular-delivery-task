78481859164762de789654022fe39298e394c126
RicardoBap <ric_bap@hotmail.com>
2021-11-11 15:01:14 -0300

    Adding TaskService and Dependency Injection

---
+++ src/app/tasks/shared/task.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+import { Task } from "./task.model";
+
+const TASKS: Array<Task> = [
+  { id: 1, title: 'fazer tarefa 1' },
+  { id: 2, title: 'fazer tarefa 2' },
+  { id: 3, title: 'fazer tarefa 3' },
+  { id: 4, title: 'fazer tarefa 4' },
+  { id: 5, title: 'fazer tarefa 5' },
+  { id: 6, title: 'fazer tarefa 6' },
+  { id: 7, title: 'fazer tarefa 7' }
+]
+
+@Injectable()
+export class TaskService {
+
+  getTasks(): Array<Task> {
+    return TASKS
+  }
+
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,31 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 
 import { Task } from "./shared/task.model";
-
-const TASKS: Array<Task> = [
-  { id: 1, title: 'fazer tarefa 1' },
-  { id: 2, title: 'fazer tarefa 2' },
-  { id: 3, title: 'fazer tarefa 3' },
-  { id: 4, title: 'fazer tarefa 4' },
-  { id: 5, title: 'fazer tarefa 5' },
-  { id: 6, title: 'fazer tarefa 6' },
-  { id: 7, title: 'fazer tarefa 7' }
-]
+import { TaskService } from "./shared/task.service";
 
 @Component({
   selector: 'tasks',
   templateUrl: './tasks.component.html',
-  styles: [ `.btn-info { margin-right: 5px; }` ]  
-  
+  styles: [ `.btn-info { margin-right: 5px; }` ],
+  providers: [
+    { provide: TaskService, useClass: TaskService }
+  ]
 })
-export class TasksComponent implements OnInit {
-  tasks: Task[]
-  selectedTask: Task  
 
-  constructor() {}
+export class TasksComponent implements OnInit {
+  tasks: Array<Task>
+  selectedTask: Task
+
+  constructor(private taskService: TaskService) {}
 
   ngOnInit() {
-   this.tasks = TASKS
+    this.tasks = this.taskService.getTasks()
   }
 
   onSelect(task: Task): void {
