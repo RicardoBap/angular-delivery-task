5a8b4386e7aa85655d3fb94da53b27f5bda10388
RicardoBap <ric_bap@hotmail.com>
2021-11-13 09:20:51 -0300

    Adding HttpClientModule to AppModule and HttpClient to TaskService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 // IMPORT MODULES
@@ -29,6 +30,7 @@ import { TaskService } from './tasks/shared/task.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     RouterModule
   ],
   providers: [{ provide: TaskService, useClass: TaskService }],
