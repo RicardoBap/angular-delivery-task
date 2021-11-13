de69ddf0dfe61bc3c1687d44b918eb3824984164
RicardoBap <ric_bap@hotmail.com>
2021-11-13 09:11:41 -0300

    Adding AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DasboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+
+const ROUTES: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DasboardComponent},
+  { path: 'tasks', component: TasksComponent },
+  { path: 'task/:id', component: TaskDetailComponent }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
+// ANGULAR IMPORTS
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+// IMPORT MODULES
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+// IMPORT COMPONENTS
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DasboardComponent } from './dashboard/dashboard.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
+// IMPORTS SERVICES
 import { TaskService } from './tasks/shared/task.service';
-
-const ROUTES = RouterModule.forRoot([
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DasboardComponent},
-  { path: 'tasks', component: TasksComponent },
-  { path: 'task/:id', component: TaskDetailComponent }
-])
 
 @NgModule({
   declarations: [
@@ -32,8 +29,7 @@ const ROUTES = RouterModule.forRoot([
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule,
-    ROUTES
+    RouterModule
   ],
   providers: [{ provide: TaskService, useClass: TaskService }],
   bootstrap: [AppComponent]
