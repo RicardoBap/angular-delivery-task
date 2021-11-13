5ae1634ef29ad32d6de58a2019777667d6e9e1e0
RicardoBap <ric_bap@hotmail.com>
2021-11-13 06:49:04 -0300

    Adding dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { DasboardComponent } from './dashboard/dashboard.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
 const ROUTES = RouterModule.forRoot([
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DasboardComponent},
   { path: 'tasks', component: TasksComponent }
 ])
 
 @NgModule({
   declarations: [
     AppComponent,
+    DasboardComponent,
     NavbarComponent,
     TasksComponent,
     TaskDetailComponent
