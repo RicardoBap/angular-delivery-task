63dd2c511f8314c2fb6682551b147e1270e0df7c
RicardoBap <ric_bap@hotmail.com>
2021-11-15 18:04:56 -0300

    Hiding navbar items  when the user is not logged

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from "@angular/core";
 
+import { AuthService } from "../shared/auth.service";
+
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html'
 })
 
 export class NavbarComponent {
+
+  constructor(public authService: AuthService) {}
 }
