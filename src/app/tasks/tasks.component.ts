e76f25fb5dfc92076804c3c42d183625fc9880b2
RicardoBap <ric_bap@hotmail.com>
2022-07-07 19:00:18 -0300

    Ajustando pagina de tasks para bootstrap 5

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,7 +9,7 @@ import { take } from "rxjs/operators";
 @Component({
   selector: 'tasks',
   templateUrl: './tasks.component.html',
-  styles: [ `.btn-info { margin-right: 5px; }` ]
+  styles: [`.btn-info { margin-right: 5px; } .bi { color: white }`]
 })
 
 export class TasksComponent implements OnInit {
@@ -23,40 +23,41 @@ export class TasksComponent implements OnInit {
   ngOnInit() {
     this.taskService.getAll()
       .subscribe({
-        next: (tasks) => { 
-          this.tasks = tasks.sort((a: { id: number; }, b: { id: number; }) => b.id - a.id) 
+        next: (tasks) => {
+          this.tasks = tasks.sort((a: { id: number; }, b: { id: number; }) => b.id - a.id)
         },
         error: (_erro) => { alert("Ocorreu um erro no servidor, tente mais tarde") }
-    })
+      })
   }
 
   createTask() {
     this.newTask.title = this.newTask.title.trim()
 
-    if(!this.newTask.title) {
+    if (!this.newTask.title) {
       this.toastService.warning('Preencha corretamente', 'Atarefa deve ter um título', {
         timeOut: 3000,
-        positionClass : 'toast-top-center'
+        positionClass: 'toast-top-center'
       })
     } else {
       this.taskService.create(this.newTask)
         .subscribe({
-          next: (task) => { this.tasks.unshift(task),
-            this.newTask = new Task(NaN, '')
-        },
+          next: (task) => {
+            this.tasks.unshift(task),
+              this.newTask = new Task(NaN, '')
+          },
           error: () => { alert("Ocorreu um erro no servidor, tente mais tarde") }
         })
     }
   }
 
   deleteTask(task: Task) {
-    this.toastService.warning('Clique aqui para confirmar', 'Deseja mesmo excluir?!',  {
+    this.toastService.warning('Clique aqui para confirmar', 'Deseja mesmo excluir?!', {
       timeOut: 6000,
-      positionClass : 'toast-top-center'
+      positionClass: 'toast-top-center'
     })
-    .onTap
-    .pipe(take(1))
-    .subscribe(() => this.toasterClickedHandler(task));
+      .onTap
+      .pipe(take(1))
+      .subscribe(() => this.toasterClickedHandler(task));
     // if( confirm(`Deseja realmente excluir a tarefa "${task.title}`) ) {
     //   this.taskService.delete(task.id)
     //     .subscribe({
@@ -70,11 +71,13 @@ export class TasksComponent implements OnInit {
     console.log('Toastr clicked');
     this.taskService.delete(task.id)
       .subscribe({
-        next: () => { this.tasks = this.tasks.filter(t => t != task),
-          this.toastService.success('204', 'Excluído com sucesso', {
-            timeOut: 1000,
-            positionClass : 'toast-top-center'
-          }) },
+        next: () => {
+          this.tasks = this.tasks.filter(t => t != task),
+            this.toastService.success('204', 'Excluído com sucesso', {
+              timeOut: 1000,
+              positionClass: 'toast-top-center'
+            })
+        },
         error: () => { alert("Ocorreu um erro no servidor, tente mais tarde") }
       })
   }
